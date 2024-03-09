d6c6c9b1c10b662ea88712b59bd54e382df46f2a
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-03-09 05:32:11 +0000

    Update SPDX license identifier and add market item structure

--- contracts/NFTMarketPlace.sol
+++ contracts/NFTMarketPlace.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
 // Internal imports for NFT OpenZeppelin contracts
@@ -8,7 +8,9 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import { Counters } from "./Counters.sol";
 
 contract NFTMarketPlace is ERC721URIStorage {
-  using Counters for Counters.Counter;
+
+// Uses the Counters library for its counters
+  using Counters for Counters.Counter;          
 
   Counters.Counter private _tokenIds;
   Counters.Counter private _itemsSold;
@@ -16,17 +18,18 @@ contract NFTMarketPlace is ERC721URIStorage {
   uint256 listingPrice = 0.0015 ether;
 
   address payable owner;
-
-  struct MarketItem {
-    uint256 tokenId;
+// Structure to represent a market item
+  struct MarketItem {                          
+    uint256 tokenId;                            
     address payable seller;
     address payable owner;
     uint256 price;
     bool sold;
   }
-  
+  // Mapping to store market items by token ID
   mapping (uint256 => MarketItem) private idMarketItem;
 
+  // Event emitted when a market item is created
   event MarketItemCreated(
     uint256 indexed tokenId,
     address seller,
@@ -34,42 +37,41 @@ contract NFTMarketPlace is ERC721URIStorage {
     uint256 price,
     bool sold
   );
-
-  modifier onlyOwner() {
+  // Restricts function access to the owner
+  modifier onlyOwner() {                      
     require(msg.sender == owner, "Only owner can call this function");
     _;
   }
-
+  // Constructor that initializes the contract
   constructor() ERC721("NFT Metaverse Token", "NFTMP") {
-    owner == payable(msg.sender);
+    owner = payable(msg.sender);                      
   }
-
-
+  // Function to update the listing price
   function updateListingPrice(uint256 _listingPrice) public payable onlyOwner {
     listingPrice = _listingPrice;
   }
-
+ // Function to retrieve the current listing price
   function getListingPrice() public view returns (uint256) {
     return listingPrice;
   }
 
-  // CREATE NFT TOKEN FUNCTION
+  // Create a new NFT token and list it for sale
 
   function createToken(string memory tokenURI, uint256 price) public payable returns(uint256) {
     _tokenIds.increment();
 
     uint256 newTokenId = _tokenIds.current();
-
-    _mint(msg.sender, newTokenId);
+   // Mints the new token
+    _mint(msg.sender, newTokenId);                  
     _setTokenURI(newTokenId, tokenURI);
-
+  // Creates a corresponding market item
     crateMarketItem(newTokenId, price);
-
+    // Returns the token ID of the created token
     return newTokenId;
   }
 
-  // CREATE MARKET ITEM FUNCTION
-  function crateMarketItem(uint256 tokenId, uint256 price) private {
+  // Create a market item for a token
+  function crateMarketItem(uint256 tokenId, uint256 price) private { 
     require(price > 0, "Price must be at least 1");
     require(msg.value == listingPrice, "Price must be equal to listing price");
 
@@ -80,5 +82,7 @@ contract NFTMarketPlace is ERC721URIStorage {
       price,
       false
     );
+
+    _transfer(msg.sender, address(this), tokenId);
   }
 }
